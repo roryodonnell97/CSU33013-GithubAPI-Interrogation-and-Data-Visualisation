c447e33829734da3268cf4222455225fdb127247
roryodonnell97 <47148370+roryodonnell97@users.noreply.github.com>
2019-11-05 22:35:33 +0000

    Changed comments

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from github import Github
 
 # using username and password
-# g = Github("roryodonnell97", "celtic199769")
+# g = Github("username", "password")
 
 # or using an access token
 g = Github("c52c1d23393df4e91e5709f827142a94d1a2184f")
+# This needs to be changed for every commit or masked when uploaded to github
 
 # Github Enterprise with custom hostname
 # g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
