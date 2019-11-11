47bcbfbf18bd493a91e662c852548378c8a2de8f
roryodonnell97 <47148370+roryodonnell97@users.noreply.github.com>
2019-11-11 17:35:40 +0000

    Added better way to access github API, commented out old way

--- test.py
+++ test.py
@@ -1,20 +1,39 @@
 from github import Github
+import urllib2
+import json
 
 # using username and password
 # g = Github("username", "password")
 
 # or using an access token
-g = Github("c52c1d23393df4e91e5709f827142a94d1a2184f")
+# g = Github("c52c1d23393df4e91e5709f827142a94d1a2184f")
 # This needs to be changed for every commit or masked when uploaded to github
 
 # Github Enterprise with custom hostname
 # g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
 
 # Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
+# for repo in g.get_user().get_repos():
+#    print(repo.name)
 
 # user = g.get_user()
 # user.login
 # print(user.login)
 
+req = urllib2.Request("https://api.github.com/repos/roryodonnell97/CSU33013-GithubAPI-Interrogation-and-Data-Visualisation/commits")
+opener = urllib2.build_opener()
+f = opener.open(req)
+json = json.loads(f.read())
+
+# print
+# print "Here is the full json"
+# print json
+
+print
+print "Here is the first element"
+print json[0]
+
+# print
+# print "Now I'm extracting data"
+# for cmt in json:
+#   print(cmt['author']['login'])
