2ec2c1d29746fbef901d68cec163027c49ba284a
roryodonnell97 <47148370+roryodonnell97@users.noreply.github.com>
2019-11-26 17:49:02 +0000

    Create table, add date values and print values

--- test.py
+++ test.py
@@ -2,57 +2,79 @@ from github import Github
 import urllib2
 import json
 import datetime
+import sqlite3 
 
-# using username and password
-# g = Github("username", "password")
-
-# or using an access token
-# g = Github("c52c1d23393df4e91e5709f827142a94d1a2184f")
-# This needs to be changed for every commit or masked when uploaded to github
-
-# Github Enterprise with custom hostname
-# g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
-
-# Then play with your Github objects:
-# for repo in g.get_user().get_repos():
-#    print(repo.name)
-
-# user = g.get_user()
-# user.login
-# print(user.login)
-
+# Retrieve JSON from Github API
 req = urllib2.Request("https://api.github.com/repos/roryodonnell97/CSU33013-GithubAPI-Interrogation-and-Data-Visualisation/commits")
 opener = urllib2.build_opener()
 f = opener.open(req)
-json = json.loads(f.read())
+commits_json = json.loads(f.read())
 
-# print
-# print "Here is the full json"
-# print json
+req = urllib2.Request("https://api.github.com/repos/roryodonnell97/CSU33013-GithubAPI-Interrogation-and-Data-Visualisation/contributors")
+opener = urllib2.build_opener()
+f = opener.open(req)
+contibutors_json = json.loads(f.read())
 
-# print
-# print "Here is the first element"
-# print json [0]
 
-fullDate = json[0]['commit']['committer']['date']
+# Create Table
+connection = sqlite3.connect("commitTable.db")    
+crsr = connection.cursor() 
 
+crsr.execute("DROP TABLE if exists commitTable") 
+sql_command = """CREATE TABLE if not exists commitTable (  
+full_date VARCHAR(30) PRIMARY KEY,  
+date VARCHAR(20),  
+weekday VARCHAR(20),  
+time VARCHAR(20)  
+);"""
+crsr.execute(sql_command) 
+
+
+# Variables
 daysOfTheWeek = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
-year = fullDate[0:4]
-month = fullDate[5:7]
-day = fullDate[8:10]
-weekdayInt = datetime.date(int(year), int(month), int(day))    # Converts date to ints
-weekdayNumber = weekdayInt.weekday()
-weekdayString = daysOfTheWeek[weekdayNumber]
+index = 0
+number_of_commits = 0
 
-date = fullDate[0:10]
-time = fullDate[11:19]
+# Get number of commits from each contributor
+while index < 1:
+    number_of_commits += contibutors_json[index]['contributions']
+    index += 1
 
-print "Here is a piece of data from the second element"
-print "Commit Date: " + date
-print "Commit Day: " + weekdayString
-print "Commit Time: " + time
 
-# print
-# print "Now I'm extracting data"
-# for cmt in json:
-#   print(cmt['author']['login'])
+# Parse the data of each commit and add to database
+for i in range(number_of_commits):
+
+    fullDate = commits_json[i]['commit']['committer']['date']
+
+    year = fullDate[0:4]
+    month = fullDate[5:7]
+    day = fullDate[8:10]
+    weekdayInt = datetime.date(int(year), int(month), int(day))    # Converts date to ints
+    weekdayNumber = weekdayInt.weekday()
+    weekdayString = daysOfTheWeek[weekdayNumber]
+
+    date = fullDate[0:10]
+    time = fullDate[11:19]
+
+    sql_command = "INSERT INTO commitTable VALUES ('"+ fullDate + "', '" + date + "', '" + weekdayString + "', '" + time + "');"
+    crsr.execute(sql_command)
+
+    # print "Commit Number: " + str(number_of_commits - i)
+    # print "Commit Date: " + date
+    # print "Commit Day: " + weekdayString
+    # print "Commit Time: " + time
+    # print
+
+
+# Print values using sql command
+sql_command = """SELECT * FROM commitTable"""  
+crsr.execute(sql_command)
+  
+ans= crsr.fetchall()   
+for i in ans: 
+    print(i)
+
+
+# Save changes to database and close
+connection.commit()   
+connection.close() 
